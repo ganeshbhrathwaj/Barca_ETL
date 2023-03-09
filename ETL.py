e51d858a1c9dc75e56a2464c80970aa18931b542
Ganeshbharathwaj <40424499+ganeshbhrathwaj@users.noreply.github.com>
2023-03-09 12:38:15 +0530

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -1,10 +1,10 @@
 import Extract as ex
-import Load
+#import Load
 if __name__ == '__main__':
-        #ex.extract_ga()
-        #ex.yellow_red()
-        #ex.squard_stats()
-        #ex.laliga_table()
-        #ex.load_temp()
-        Load.upload()
+        ex.extract_ga()
+        ex.yellow_red()
+        ex.squard_stats()
+        ex.laliga_table()
+        ex.load_temp()
+        #Load.upload()
         
